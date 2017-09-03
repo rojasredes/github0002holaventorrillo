c7fde04590a795b8ea7dcef09900fc7676b045d2
rojasredes <31563377+rojasredes@users.noreply.github.com>
2017-09-03 11:36:20 +0200

    version 1 Hola, Ventorrillo mediante etiqueta.setText()

--- app/src/main/java/com/rojasdelgado/github0001holamundo/MainActivity.java
+++ app/src/main/java/com/rojasdelgado/github0001holamundo/MainActivity.java
@@ -1,13 +1,19 @@
-package com.rojasdelgado.github0001holamundo;
+package com.rojasdelgado.github0002holaventorrillo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView etiqueta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etiqueta=(TextView)findViewById(R.id.etiqueta);
+        etiqueta.setText("¡Hola, Ventorrillo!");
     }
 }
